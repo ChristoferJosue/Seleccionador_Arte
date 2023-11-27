ad7c231b3e1c4bbf16cd1665193c37635e313cc3
ChristoferJosue <christofer.cg8@gmail.com>
2023-11-26 19:16:46 -0500

    Agregado nombre de autor

--- app/src/main/java/com/example/parcial3/MainActivity.java
+++ app/src/main/java/com/example/parcial3/MainActivity.java
@@ -2,12 +2,11 @@ package com.example.parcial3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
@@ -39,124 +38,101 @@ public class MainActivity extends AppCompatActivity {
         Rb5=(RadioButton) findViewById(R.id.RbTema5);
         datePicker1=findViewById(R.id.BtnDatePicker);
         BtnSalir=(ImageButton) findViewById(R.id.btnSalir);
-        BtnSalir.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //finaliza todo
-                finishAffinity();
-            }
+
+        BtnSalir.setOnClickListener(View -> {//probando un nuevo metodo
+
+            finishAffinity();
         });
-        datePicker1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FechaInicial();
+
+        datePicker1.setOnClickListener(view -> FechaInicial());
+        BtnGuardar.setOnClickListener(view -> {
+            int centinela;
+            String nombreStr=EtNombre.getText().toString();
+            String apellidoStr=EtApellido.getText().toString();
+            String FechaStr=datePicker1.getText().toString();
+            if(nombreStr.isEmpty() || apellidoStr.isEmpty()){//si el nombre o el apellido estan vacios tira este mensaje
+                Toast.makeText(MainActivity.this,"El nombre o el apellido estan vacios", Toast.LENGTH_SHORT).show();
+                centinela=1;
             }
+
+            if (FechaStr.isEmpty()){//si no ha seleccionado la fecha tira este mensaje
+                Toast.makeText(MainActivity.this,"Elige la fecha",Toast.LENGTH_SHORT).show();
+                centinela=1;
+
+            }
+            else{
+                centinela=0;
+            }
+
+
+
+
         });
-        BtnGuardar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int centinela=0;
-                String nombreStr=EtNombre.getText().toString();
-                String apellidoStr=EtApellido.getText().toString();
-                String FechaStr=datePicker1.getText().toString();
-                if(nombreStr.isEmpty() || apellidoStr.isEmpty()){//si el nombre o el apellido estan vacios tira este mensaje
-                    Toast.makeText(MainActivity.this,"El nombre o el apellido estan vacios", Toast.LENGTH_SHORT).show();
-                    centinela=1;
-                }
-
-                if (FechaStr.isEmpty()){//si no ha seleccionado la fecha tira este mensaje
-                    Toast.makeText(MainActivity.this,"Elige la fecha",Toast.LENGTH_SHORT).show();
-                    centinela=1;
-
-                }
-                else{
-                    centinela=0;
-                }
+        Rb1.setOnClickListener(v -> {
+            if(Rb1.isChecked())//si este se activa o hace click desactivar los otros
+            {
+                position=0;
+               actualizarImagen(position);
 
 
 
+
+                Rb5.setChecked(false);
+                Rb2.setChecked(false);
+                Rb3.setChecked(false);
+                Rb4.setChecked(false);
             }
+
+
         });
-        Rb1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(Rb1.isChecked())//si este se activa o hace click desactivar los otros
-                {
-                    position=0;
-                   actualizarImagen(position);
-
-
-
-
-                    Rb5.setChecked(false);
-                    Rb2.setChecked(false);
-                    Rb3.setChecked(false);
-                    Rb4.setChecked(false);
-                }
-
-
+        Rb2.setOnClickListener(v -> {
+            if(Rb2.isChecked())//si este se activa o hace click desactivar los otros
+            {
+                position=1;
+                actualizarImagen(position);
+                Rb5.setChecked(false);
+                Rb1.setChecked(false);
+                Rb3.setChecked(false);
+                Rb4.setChecked(false);
             }
+
         });
-        Rb2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(Rb2.isChecked())//si este se activa o hace click desactivar los otros
-                {
-                    position=1;
-                    actualizarImagen(position);
-                    Rb5.setChecked(false);
-                    Rb1.setChecked(false);
-                    Rb3.setChecked(false);
-                    Rb4.setChecked(false);
-                }
-
+        Rb3.setOnClickListener(v -> {
+            if(Rb3.isChecked())//si este se activa o hace click desactivar los otros
+            {
+                position=2;
+                actualizarImagen(position);
+                Rb5.setChecked(false);
+                Rb2.setChecked(false);
+                Rb1.setChecked(false);
+                Rb4.setChecked(false);
             }
+
         });
-        Rb3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(Rb3.isChecked())//si este se activa o hace click desactivar los otros
-                {
-                    position=2;
-                    actualizarImagen(position);
-                    Rb5.setChecked(false);
-                    Rb2.setChecked(false);
-                    Rb1.setChecked(false);
-                    Rb4.setChecked(false);
-                }
-
+        Rb4.setOnClickListener(v -> {
+            if(Rb4.isChecked())//si este se activa o hace click desactivar los otros
+            {
+                position=3;
+                actualizarImagen(position);
+                Rb5.setChecked(false);
+                Rb2.setChecked(false);
+                Rb3.setChecked(false);
+                Rb1.setChecked(false);
             }
+
         });
-        Rb4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(Rb4.isChecked())//si este se activa o hace click desactivar los otros
-                {
-                    position=3;
-                    actualizarImagen(position);
-                    Rb5.setChecked(false);
-                    Rb2.setChecked(false);
-                    Rb3.setChecked(false);
-                    Rb1.setChecked(false);
-                }
+        Rb5.setOnClickListener(v -> {
+            if(Rb5.isChecked())//si este se activa o hace click desactivar los otros
+            {
 
+                position=4;
+                actualizarImagen(position);
+                Rb1.setChecked(false);
+                Rb2.setChecked(false);
+                Rb3.setChecked(false);
+                Rb4.setChecked(false);
             }
-        });
-        Rb5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(Rb5.isChecked())//si este se activa o hace click desactivar los otros
-                {
 
-                    position=4;
-                    actualizarImagen(position);
-                    Rb1.setChecked(false);
-                    Rb2.setChecked(false);
-                    Rb3.setChecked(false);
-                    Rb4.setChecked(false);
-                }
-
-            }
         });
 
 
@@ -165,20 +141,21 @@ public class MainActivity extends AppCompatActivity {
     private void actualizarImagen(int position) {
         // Establece la imagen del ImageButton según la posición que se da en parametro
         BtnImagenes.setVisibility(View.VISIBLE);
-        BtnImagenes.setImageDrawable(getResources().getDrawable(imagen[position]));
+        BtnImagenes.setImageDrawable(getResources().getDrawable(imagen[position], getTheme()));
+
+
 
     }
     private void FechaInicial(){
         //
-        DatePickerDialog dialog= new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                //guardar los datos que la persona puso en variables para su posterior uso
-                anioPersona=year;
-                mesPersona=month+1;
-                diaPersona=day;
-                datePicker1.setText(String.valueOf(day)+"/"+String.valueOf(month+1)+"/"+String.valueOf(year));//texto que se va mostrar ;
-            }
+        @SuppressLint("SetTextI18n")//sirve para que ignore la advertencia para el metodo settextindicando
+        // que estás consciente de la situación y has decidido no aplicar internacionalización en ese caso particular.
+        DatePickerDialog dialog= new DatePickerDialog(this, (datePicker, year, month, day) -> {
+            //guardar los datos que la persona puso en variables para su posterior uso
+            anioPersona=year;
+            mesPersona=month+1;
+            diaPersona=day;
+            datePicker1.setText(day +"/"+ (month + 1) +"/"+ year);//texto que se va mostrar ;
         }, 2023, 11, 26);
         dialog.show();
     }
